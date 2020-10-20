9b1160db43ef8e4546f3caad1dc8024b2b58d832
Roman Kulich <rpkulich@gmail.com>
2020-10-20 15:38:38 +0200

    Update wpvscan.py

--- wpvscan.py
+++ wpvscan.py
@@ -22,5 +22,5 @@ WP_version = WP_pars[10:15] + "."
 WP_now = "5.5.1." # Manually added from https://api.wordpress.org/core/version-check/1.7/
 
 print(" ")
-print("Website " + website + " is running on CMS " + WP_name + " of version " +WP_version)
+print("Website " + website + " is running on CMS " + WP_name + " of version " + WP_version)
 print("Latest version is " + WP_now)
