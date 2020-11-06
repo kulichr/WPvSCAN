8f7ca01c4527f386340bd4ac7882c887f18433ca
Roman Kulich <rpkulich@gmail.com>
2020-11-06 22:38:24 +0100

    Argparse added

--- wpvscan.py
+++ wpvscan.py
@@ -2,11 +2,15 @@
 # Name: WPvSCAN
 # https://github.com/cyb3rd3s/WPvSCAN
 # Author: Roman Kulich @ 2020
-# Version: v1.0.2
+# Version: v1.0.3
 import bs4 as bs
 import urllib.request
 import time
 import os
+import argparse
+
+TGREEN =  '\033[32m' # Green Text
+TWHITE = '\033[37m'
 
 print('''
  __          _______         _____  _____          _   _ 
@@ -17,7 +21,14 @@ print('''
      \/  \/   |_|      \_/ |_____/ \_____/_/    \_\_| \_|                                                                                                                
 v1.0.0
 ''')
-website = input("Enter website: ")
+print(TGREEN + "USAGE: wpvscan.py target.com", TWHITE)
+print("")
+
+parser = argparse.ArgumentParser(description="Human Para")
+parser.add_argument(dest='domain', help="wpvscan.py target.com")
+args = parser.parse_args()
+
+website = (args.domain)
 source = urllib.request.urlopen('https://'+ website).read()
 soup = bs.BeautifulSoup(source,'lxml')
 WP_check = soup.find(attrs={'name' : 'generator'})
@@ -30,7 +41,7 @@ current_time = time.strftime("%H:%M:%S", t)
 
 print(" ")
 print("Target website " + website + " is running on CMS " + WP_name + " of version " +WP_version)
-print("Latest version is " + WP_now)
+print("Latest version is " + TGREEN + WP_now,TWHITE)
 print("Scan finished " + current_time)
 
 searchsploit = input("Do you want to use searchsploit to check exploits for this version? (y/n) ")
@@ -38,4 +49,4 @@ if searchsploit == "y":
     print(" ")
     print(os.system("searchsploit " + WP_pars))
 else:
-    print("Finished")
+    print("DONE")
