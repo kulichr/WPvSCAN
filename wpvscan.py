d0263e45fc35496a70ce61fe3b17e74216a70f67
Roman Kulich <rpkulich@gmail.com>
2020-11-10 23:37:01 +0100

    Auto checking with JSON from WordPress.org API

--- wpvscan.py
+++ wpvscan.py
@@ -8,9 +8,11 @@ import urllib.request
 import time
 import os
 import argparse
+import requests
 
 TGREEN =  '\033[32m' # Green Text
-TWHITE = '\033[37m'
+TWHITE = '\033[37m' # White text
+TRED = '\033[31m' # White text
 
 print('''
  __          _______         _____  _____          _   _ 
@@ -24,6 +26,9 @@ v1.0.0
 print(TGREEN + "USAGE: wpvscan.py target.com", TWHITE)
 print("")
 
+response = requests.get('https://api.wordpress.org/core/version-check/1.7/')
+json = response.json()
+
 parser = argparse.ArgumentParser(description="Human Para")
 parser.add_argument(dest='domain', help="wpvscan.py target.com")
 args = parser.parse_args()
@@ -35,12 +40,15 @@ WP_check = soup.find(attrs={'name' : 'generator'})
 WP_pars = WP_check['content']
 WP_name = WP_pars[0:9]
 WP_version = WP_pars[10:15] + "." 
-WP_now = "5.5.1." # Manually added from https://api.wordpress.org/core/version-check/1.7/
+WP_now = str(json['offers'][0]['version'])
 t = time.localtime()
 current_time = time.strftime("%H:%M:%S", t)
 
 print(" ")
-print("Target website " + website + " is running on CMS " + WP_name + " of version " +WP_version)
+if WP_version == WP_now:
+    print("Target website " + website + " is running on CMS " + WP_name + " of version " + TGREEN + WP_version,TWHITE)
+else:
+    print("Target website " + website + " is running on CMS " + WP_name + " of version " + TRED + WP_version,TWHITE)
 print("Latest version is " + TGREEN + WP_now,TWHITE)
 print("Scan finished " + current_time)
 
